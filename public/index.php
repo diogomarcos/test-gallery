580466c533d7e132c9859590d486b6bc478778bf
Diogo Marcos <contato@diogomarcos.com>
2017-06-15 18:39:09 -0300

    [#3] Setting the Index for system operation

--- public/index.php
+++ public/index.php
@@ -1,3 +1,21 @@
 <?php
-require_once __DIR__."/../vendor/autoload.php";
-echo "ok";
+/**
+ * index
+ *
+ * @author  Diogo Marcos <contato@diogomarcos.com>
+ * @version 1.0
+ */
+
+session_start();
+
+use App\Models\Gallery;
+use App\Route;
+use Micro\DependencyInjection\Container;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+/** @var Gallery $gallery */
+$gallery = Container::getModel('Gallery');
+$gallery->createTable();
+
+$route = new Route();
